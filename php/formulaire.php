f6942ba3e4ac2507413cc42af6226cc4145a9527
Painteur3 <75747108+Painteur3@users.noreply.github.com>
2022-06-23 11:24:02 +0200

    Delete php directory

--- php/formulaire.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $retour = mail('gab.marcq3@gmail.com', 'Envoi depuis la page Contact', $_POST['message'], "");
-    if ($retour)
-        echo '<p>Votre message a bien été envoyé.</p>';
-    ?>
